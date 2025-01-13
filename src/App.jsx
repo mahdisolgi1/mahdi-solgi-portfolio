0ced9b6e58af436d129a63fe2f77fdfe678b2660
MahdiSolgi <vass1383@gmail.com>
2025-01-13 12:45:49 -0800

    feat(App): create App layouts

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="fixed -z-10 min-h-screen w-full [background:radial-gradient(125%_125%_at_50%_10%,#d2d4eb_40%,#4ca9ce_100%)] dark:[background:radial-gradient(125%_125%_at_50%_10%,#000000_40%,#4ca9ce_100%)]"></div>
+
+      <main className="flex flex-col items-center px-4 md:px-8 lg:px-16 "></main>
+    </>
+  );
+}
+
+export default App;
