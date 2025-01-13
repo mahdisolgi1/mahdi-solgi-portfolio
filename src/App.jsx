b954c8c2406d2cefd2e441214e9dcd25bbf56572
MahdiSolgi <vass1383@gmail.com>
2025-01-13 12:57:38 -0800

    feat(App): add Hero Component to App page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Navbar from "./Components/Navbar";
+import Hero from "./Components/Hero";
 
 function App() {
   return (
     <>
       <div className="fixed -z-10 min-h-screen w-full [background:radial-gradient(125%_125%_at_50%_10%,#d2d4eb_40%,#4ca9ce_100%)] dark:[background:radial-gradient(125%_125%_at_50%_10%,#000000_40%,#4ca9ce_100%)]"></div>
-      <Navbar />
-      <main className="flex flex-col items-center px-4 md:px-8 lg:px-16 "></main>
+
+      <main className="flex flex-col items-center px-4 md:px-8 lg:px-16 ">
+        <Navbar />
+        <Hero />
+      </main>
     </>
   );
 }
