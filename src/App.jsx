e5bd09c3838ee9a2084821cdda8a823bb98f15d6
MahdiSolgi <vass1383@gmail.com>
2025-01-13 14:02:16 -0800

    feat(App): add Footer component to App page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
 import Tech from "./Components/Tech";
 import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Tech />
         <Contact />
       </main>
+      <Footer />
     </>
   );
 }
