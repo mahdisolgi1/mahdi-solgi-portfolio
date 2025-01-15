0a9992f1df71e7e052f4bb13e432feab014798ab
MahdiSolgi <vass1383@gmail.com>
2025-01-15 14:14:02 -0800

    styles(Footer): add more bottom padding because of down bar

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,4 +1,6 @@
 import { motion } from "motion/react";
+import { BiLogoGmail } from "react-icons/bi";
+import { BsGithub, BsLinkedin } from "react-icons/bs";
 
 const Footer = () => {
   return (
@@ -6,7 +8,7 @@ const Footer = () => {
       initial={{ y: 80, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
       transition={{ duration: 0.8, delay: 0.2 }}
-      className="bottom-0 flex items-center justify-around w-full px-16 py-6 border-b border-b-gray-700 bg-black/80 dark:bg-white/80 text-gray-50 dark:text-gray-950 backdrop-blur-md md:justify-between"
+      className="bottom-0 flex items-center justify-between w-full px-16 pt-6 border-b pb-28 md:pb-6 border-b-gray-700 bg-black/80 dark:bg-white/80 text-gray-50 dark:text-gray-950 backdrop-blur-md md:justify-between"
     >
       <ul className="hidden gap-5 md:flex md:gap-10">
         <a
@@ -34,9 +36,33 @@ const Footer = () => {
           <li>Contact</li>
         </a>
       </ul>
-      <p className="text-sm md:text-base">
-        © 2025 Mahdi Solgi. All rights reserved.
-      </p>
+      <ul className="flex gap-5 md:hidden">
+        <li>
+          <a
+            href="https://www.linkedin.com/in/mahdi-solgi/"
+            className="text-xl transition-all duration-300 cursor-pointer opacity-70 hover:opacity-100"
+          >
+            <BsLinkedin />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/mahdisolgi1"
+            className="text-xl transition-all duration-300 cursor-pointer opacity-70 hover:opacity-100"
+          >
+            <BsGithub />
+          </a>
+        </li>
+        <li>
+          <a
+            href="mailto:mahdi.solgi.dev@gmail.com"
+            className="text-xl transition-all duration-300 cursor-pointer opacity-70 hover:opacity-100"
+          >
+            <BiLogoGmail />
+          </a>
+        </li>
+      </ul>
+      <p className="text-sm md:text-base">© 2025 Mahdi Solgi.</p>
     </motion.footer>
   );
 };
